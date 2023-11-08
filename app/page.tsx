35d8146a95b214e2cca91f802867a39f6c26d153
One Chowdhury <coffeeandone@gmail.com>
2023-11-07 22:46:55 -0500

    api setup done

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,30 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+
+const handleQuery = () => {
+  fetch("https://api.vectara.io:443/v1/query", {
+    headers: {
+      "Content-Type": "application/json",
+      "x-api-key": "zwt_7UYiqRAuYx3q43vUQa47TqwD-_xVPlfAHljLmg",
+      "customer-id": "3980796585",
+    },
+    body: JSON.stringify({
+      query: [
+        {
+          query: "TELL ME ABOUT BRAIN?",
+          num_results: 10,
+          corpus_key: [{ customer_id: "3980796585", corpus_id: "6" }],
+        },
+      ],
+    }),
+    method: "post",
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
+};
 
 export default function Home() {
   return (
@@ -7,7 +32,12 @@ export default function Home() {
       <h1>Radiology findings</h1>
       <h1>Relevant information & resources</h1>
 
-      <Button className="max-w-sm">Analzye</Button>
+      <Button
+        onClick={handleQuery}
+        className="max-w-sm"
+      >
+        Analzye
+      </Button>
       <div>PDF name</div>
     </main>
   );
